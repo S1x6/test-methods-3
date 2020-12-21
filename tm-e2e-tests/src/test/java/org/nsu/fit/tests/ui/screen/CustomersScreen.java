d182256d1818e11d3bbb2c789ea61a7a836a6f41
Vadim <v.korshunov@g.nsu.ru>
2020-12-21 12:13:47 +0700

    ui tests

--- tm-e2e-tests/src/test/java/org/nsu/fit/tests/ui/screen/CustomersScreen.java
+++ tm-e2e-tests/src/test/java/org/nsu/fit/tests/ui/screen/CustomersScreen.java
@@ -79,7 +79,7 @@ public class CustomersScreen implements Screen {
         list.get(0).findElements(By.tagName("td")).get(0).click();
 
         try {
-            Thread.sleep(500);
+            Thread.sleep(1000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
